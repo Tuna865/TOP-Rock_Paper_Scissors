9b758327c715f25fa232c4b355a53cd5ef0adb34
Turner Milan <turnermilan@gmail.com>
2022-08-15 21:45:53 -0400

    completed computer choice function

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-const getComputerChoice = () => {
+let options = {
+    rock: 'rock',
+    paper: 'paper',
+    scissors: 'scissors'
+}
+
+// returns a random choice from the 3 options in RPS
+const getComputerChoice = (obj) => {
+    const plays = Object.keys(obj)
+    return plays[Math.floor(Math.random() * plays.length)]
+}
+
+// console.log(getComputerChoice(options))
+
+// probably gonna need several conditionals for this 
+const round = (compSelect, playerSelect) =>{
     
 }
