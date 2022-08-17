f3eac8cd3aaa2bdb16762c41feeab244e635a9a2
Turner Milan <turnermilan@gmail.com>
2022-08-16 21:33:42 -0400

    worked on playerChoice function and made to do list

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+/* To Do
+
+Need a way to establish that:
+    rock beats scissors 
+    scissors beats paper
+    paper beats rock
+    might could just declare win conditions
+Find a way to see what the player chose: 
+    might do something other than prompt, 
+    such as an input or form since I have used that before
+Create comparisons:
+    one for when the player & computer choose the same
+    one for player winner 
+    one for comp winner 
+
+*/
 let options = {
     rock: 'rock',
     paper: 'paper',
@@ -6,13 +22,36 @@ let options = {
 
 // returns a random choice from the 3 options in RPS
 const getComputerChoice = (obj) => {
-    const plays = Object.keys(obj)
-    return plays[Math.floor(Math.random() * plays.length)]
+    const choice = Object.keys(obj)
+    return choice[Math.floor(Math.random() * choice.length)]
 }
+console.log(getComputerChoice(options))
 
-// console.log(getComputerChoice(options))
+const getPlayerChoice = () => {
+    let playerChoice = prompt('Enter your choice of rock, paper, or scissors: ')
+    playerChoice.toLowerCase()
+    return playerChoice
+}
+getPlayerChoice()
 
 // probably gonna need several conditionals for this 
-const round = (compSelect, playerSelect) =>{
+// cant decide if I should do switch & case or if else statements
+const round = (compSelection, playerSelection) =>{
+    let compChoice = getComputerChoice(options)
+    let playerChoice = getPlayerChoice()
     
+}
+
+
+
+
+switch(choice) {
+    case draw:
+        console.log('draw game');
+        break
+    case loser: 
+        console.log('computer wins :/')
+        break
+    case winner:
+        console.log('you win!')
 }
