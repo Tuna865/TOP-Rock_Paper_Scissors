fb8543e9011ff320ed1c1e3e1dfbc60a4908c03b
Turner Milan <turnermilan@gmail.com>
2022-08-20 13:03:41 -0400

    completed round function

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 /* To Do
 
-Complete the round function
-    this is working to some extent, need to fix some errors
-Complete the game function
 Add a reset game button
     gonna look at my Arcade project for this
 
@@ -24,8 +21,8 @@ const getComputerChoice = () => {
     let compChoice = options[Math.floor(Math.random() * options.length)]
     return compChoice
 }
-console.log("computer choice:",getComputerChoice(options))
 
+// chose to use this method for user input instead of a prompt
 buttonChoice.forEach((button) =>{
     button.addEventListener("click", () => {
         const selection = button.querySelector("img")
@@ -38,36 +35,32 @@ buttonChoice.forEach((button) =>{
     })
 })
 
-
 // probably gonna need several conditionals for this 
 const playRound = (compChoice, playerChoice) =>{
-    
     if(compChoice == playerChoice){
-        console.log('Tie game, no points awarded')
+        console.log('Tie round, no points awarded', playerScore, compScore)
     }else if(
     // round win conditions for comp (therefore, otherwise the player gets a point)
         (compChoice == 'rock' && playerChoice == 'scissors') ||
         (compChoice == 'scissors' && playerChoice == 'paper') ||
         (compChoice == 'paper' && playerChoice == 'rock')
-    ){ ++compScore;
+    ){ ++compScore 
+        console.log("Computer scores", compScore, playerScore);
         if(compScore == 5){
             console.log('Box of enslaved lighning wins :/')
+            reset()
         }
-    }else{++playerScore;
+    }else{++playerScore
+        console.log("Player scores", compScore, playerScore);
         if(playerScore == 5){
             console.log("That's a dub for humans!")
+            reset()
         }
     }
 }
 
-const game = () => {
-    playRound(compChoice, playerChoice)
-    for(let i = 0; i < 5; i++){
-        if(compChoice === playerChoice){
-            console.log('Draw game')
-        } else if (compChoice > playerChoice){
+const reset = () => {
+    let playerScore =0
+    let compScore =0
 
-        }
-    }
 }
-
